5298a9b958db26322aefcae37a4972ae2031fb1d
allemkarthik <allemkarthik@gmail.com>
2025-11-07 23:43:56 -0800

    day1

---
+++ script.js
@@ -0,0 +1,16 @@
+const parent = React.createElement(
+  "div",
+  { class: "parent" },
+  React.createElement("div", { class: "child" }, [
+    React.createElement("h1", {}, "h1 tag"),
+    React.createElement("h2", {}, "h2 tag"),
+  ])
+);
+
+const headind = React.createElement(
+  "h1",
+  { id: "heading" },
+  "hello from react"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
